38b8f358787844fd7a28dc291a9e5bd3212b3987
Newinfinite007 <ravi@glib.ai>
2018-07-20 21:20:53 +0530

    smallest and nearest number to the left in an array of integers

---
+++ Smallest-Nearest-Left/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void smallestNearestLeft(const int *arr, int size){
+
+    for (int i = 0; i < size; ++i) {
+        int j = i;
+        while(arr[j]>=arr[i] and j>-1)
+            --j;
+        if(j==-1)
+            cout<<"_"<<"\t";
+        else
+            cout<<arr[j]<<"\t";
+    }
+}
+
+int main() {
+//    int arr[] = {1, 6, 4, 10, 2, 5}, arr_size=6;
+    int arr[] = {1, 3, 0, 2, 5}, arr_size=6;
+    smallestNearestLeft(arr, arr_size);
+    cout<<endl;
+    return 0;
+}
