679e9a6fe7a436dc5a686a805cb2361d74069ce6
infinite007 <cs12b1031@iith.ac.in>
2018-07-20 21:28:47 +0530

    changes in printing output

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "cmake-build-debug/stack.cpp"
+#include "cmake-build-debug/string_utils.cpp"
+#include "cmake-build-debug/universe_script.cpp"
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+//    int t = 0;
+//    cin>>t;
+//    for (int i = 0; i < t; ++i) {
+//        string p;
+//        int damage, minHacks;
+//        cin>>damage;
+//        cin>>p;
+//        minHacks = hack3(p, damage);
+//        if(minHacks==-1)
+//            cout<<"Case #"<<(i+1)<<": IMPOSSIBLE"<<endl;
+//        else
+//            cout<<"Case #"<<(i+1)<<": "<<minHacks<<endl;
+//    }
+//    return 0;
+    int x = 1000000000;
+    cout<<"hi"<<x<<endl;
+    return 0;
+}
+
